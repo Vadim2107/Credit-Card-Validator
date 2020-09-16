a4dedcbeacd9222520a71392b07668f307297c0a
Ann Vasilyeva <skazkadreamanna@gmail.com>
2020-09-16 19:04:51 +0300

    eslint fix

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -3,44 +3,40 @@ import puppetteer from 'puppeteer';
 jest.setTimeout(30000); // default puppeteer timeout
 
 describe('Credit Card Validator form', () => {
-    let browser = null;
-    let page = null;
-    const baseUrl = 'http://localhost:9000';
+  let browser = null;
+  let page = null;
+  const baseUrl = 'http://localhost:9000';
 
-    beforeAll(async () => {
-        browser = await puppetteer.launch({
-            headless: false, // show gui
-            slowMo: 250,
-            devtools: true, // show devTools
-        });
-        page = await browser.newPage();
+  beforeAll(async () => {
+    browser = await puppetteer.launch({
+      headless: false, // show gui
+      slowMo: 250,
+      devtools: true, // show devTools
     });
+    page = await browser.newPage();
+  });
 
-    afterAll(async () => {
-        await browser.close();
-    });
+  afterAll(async () => {
+    await browser.close();
+  });
 
+  test('should add .valid class for valid card', async () => {
+    await page.goto(baseUrl);
+    const form = await page.$('[data-widget="card-num-form-widget"]');
+    const input = await form.$('[data-id="card-num-input"]');
+    await input.type('4222222222222');
+    const submit = await form.$('[data-id="card-num-submit"]');
+    submit.click();
+    await page.waitForSelector('[data-id="card-num-input"].valid');
+  });
 
-    test('should add .valid class for valid card', async () => {
-        await page.goto(baseUrl);
-        const form = await page.$('[data-widget="card-num-form-widget"]');
-        const input = await form.$('[data-id="card-num-input"]');
-        await input.type('4222222222222');
-        const submit = await form.$('[data-id="card-num-submit"]');
-        submit.click();
-        await page.waitForSelector('[data-id="card-num-input"].valid');
-    });
-
-    test('should remove .valid  add .invalid class for invalid card', async () => {
-        await page.goto(baseUrl);
-        const form = await page.$('[data-widget="card-num-form-widget"]');
-        const input = await form.$('[data-id="card-num-input"]');
-        await input.type('42222222');
-        const submit = await form.$('[data-id="card-num-submit"]');
-        submit.click();
-        await page.waitForSelector('[data-id="card-num-input"].invalid');
-    });
-
-
-
+  test('should remove .valid  add .invalid class for invalid card', async () => {
+    await page.goto(baseUrl);
+    const form = await page.$('[data-widget="card-num-form-widget"]');
+    const input = await form.$('[data-id="card-num-input"]');
+    await input.type('42222222');
+    const submit = await form.$('[data-id="card-num-submit"]');
+    submit.click();
+    await page.waitForSelector('[data-id="card-num-input"].invalid');
+  });
 });
